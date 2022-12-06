3dae00bb9c998adfcc06c6388aa85be5797b03fc
Lucas Lacerda <lucaslacerdacl@gmail.com>
2022-12-05 22:48:32 -0300

    #deploy

--- tests/integration/jest.config.ts
+++ tests/integration/jest.config.ts
@@ -14,6 +14,7 @@ const config: Config.InitialOptions = {
   coverageDirectory: 'coverage/integration',
   testMatch: ['<rootDir>/tests/integration/**/*.spec.ts'],
   setupFiles: ['<rootDir>/tests/integration/setup-env.ts'],
+  setupFilesAfterEnv: ['<rootDir>/tests/integration/setup-database.ts'],
 };
 
 export default config;
